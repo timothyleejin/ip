30dfb9dc5165fdd025cdd39b2816ee867220ac1c
timothyleejin <timothyleejin@gmail.com>
2025-08-21 23:52:19 +0800

    Added echo function

--- src/main/java/Siri.java
+++ src/main/java/Siri.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
+
 public class Siri {
     public static void main(String[] args) {
-        System.out.println
-                ("Hello! I'm Siri!\n" +
-                "What can I do for you?\n" +
-                "Bye! Hope to see you again soon!\n");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello! I'm Siri!\n");
+        System.out.println("What can I do for you?\n");
+
+        while (true) {
+            String command = scanner.nextLine();
+            if (command.equalsIgnoreCase("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(command);
+            }
+        }
+        scanner.close();
     }
 }
