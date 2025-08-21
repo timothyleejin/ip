178bbd490959d5adf4e1a9cc02210a3b33250ced
timothyleejin <timothyleejin@gmail.com>
2025-08-22 00:25:19 +0800

    Added mark task as done function

--- src/main/java/Siri.java
+++ src/main/java/Siri.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Siri {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskNumber = 0;
 
         System.out.println("Hello! I'm Siri!\n");
@@ -11,18 +11,41 @@ public class Siri {
 
         while (true) {
             String command = scanner.nextLine();
+
             if (command.equalsIgnoreCase("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
-            } else if (command.equalsIgnoreCase("list")) {
+            }
+
+            else if (command.equalsIgnoreCase("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskNumber; i++) {
                     System.out.println((i + 1) + ". " + tasks[i]);
                 }
-            } else {
-                tasks[taskNumber] = command;
+            }
+
+            else if (command.startsWith("mark ")) {
+                String[] word = command.split(" ");
+                int index = Integer.parseInt(word[1]) - 1;
+                tasks[index].markDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  " + tasks[index]);
+            }
+
+            else if (command.startsWith("unmark ")) {
+                String[] word = command.split(" ");
+                int index = Integer.parseInt(word[1]) - 1;
+                tasks[index].markUndone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("  " + tasks[index]);
+            }
+
+            else {
+                tasks[taskNumber] = new Task(command);
                 taskNumber++;
                 System.out.println("added: " + command);
             }
+
         }
         scanner.close();
     }
