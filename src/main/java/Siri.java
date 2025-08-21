eb82d989714b9d1ca6acf0d6c9bc42bcc7706fd5
timothyleejin <timothyleejin@gmail.com>
2025-08-21 23:36:20 +0800

    Level-0: Rename, Greet, Exit

---
+++ src/main/java/Siri.java
@@ -0,0 +1,8 @@
+public class Siri {
+    public static void main(String[] args) {
+        System.out.println
+                ("Hello! I'm Siri!\n" +
+                "What can I do for you?\n" +
+                "Bye! Hope to see you again soon!\n");
+    }
+}
