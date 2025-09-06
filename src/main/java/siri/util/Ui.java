6863702b268ca89d478810f4c9f0bb971c75255d
timothyleejin <timothyleejin@gmail.com>
2025-09-06 10:50:26 +0800

    Edit method names to follow java standard

--- src/main/java/siri/util/Ui.java
+++ src/main/java/siri/util/Ui.java
@@ -9,53 +9,53 @@ public class Ui {
         System.out.println("What can I do for you?\n");
     }
 
-    private void borderLine() {
+    private void printborderLine() {
         System.out.println("____________________________________________________________");
     }
 
     public void sayGoodbye() {
-        borderLine();
+        printborderLine();
         System.out.println("Bye bye :( Hope to see you again soon!");
-        borderLine();
+        printborderLine();
     }
 
     public void sayError(String message) {
-        borderLine();
+        printborderLine();
         System.out.println(" " + message);
-        borderLine();
+        printborderLine();
     }
 
     public void sayTaskAdded(Task task, int size) {
-        borderLine();
+        printborderLine();
         System.out.println("Got it :)) I've added this task:\n " + task);
         System.out.println("Now you have " + size + " task(s) in the list.");
-        borderLine();
+        printborderLine();
     }
 
     public void sayTaskList(List<Task> tasks) {
-        borderLine();
+        printborderLine();
         System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < tasks.size(); i++) {
             System.out.println((i + 1) + "." + tasks.get(i));
         }
-        borderLine();
+        printborderLine();
     }
 
     public void sayTaskMarked(Task task, boolean isDone) {
-        borderLine();
+        printborderLine();
         if (isDone) {
             System.out.println("Nice! I've marked this task as done:\n " + task);
         } else {
             System.out.println("OK, I've marked this task as not done yet:\n " + task);
         }
-        borderLine();
+        printborderLine();
     }
 
     public void sayTaskDeleted(Task task, int size) {
-        borderLine();
+        printborderLine();
         System.out.println("Gotcha :) I've removed this task:\n " + task);
         System.out.println("Now you have " + size + " tasks in the list.");
-        borderLine();
+        printborderLine();
     }
 
     /**
@@ -65,7 +65,7 @@ public class Ui {
      * @param keyword the search term that was used.
      */
     public void sayMatchingTasks(List<Task> matchingTasks, String keyword) {
-        borderLine();
+        printborderLine();
         if (matchingTasks.isEmpty()) {
             System.out.println("Sorry! No tasks found containing: '" + keyword + "'");
         } else {
@@ -74,6 +74,6 @@ public class Ui {
                 System.out.println((i + 1) + "." + matchingTasks.get(i));
             }
         }
-        borderLine();
+        printborderLine();
     }
 }
