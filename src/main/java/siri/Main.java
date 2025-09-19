d22bed2716ff2da2bba59a8db5ab4c591dc0984b
timothyleejin <timothyleejin@gmail.com>
2025-09-19 12:49:30 +0800

    Add Siri title to GUI

--- src/main/java/siri/Main.java
+++ src/main/java/siri/Main.java
@@ -22,6 +22,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Siri chatbot :)");
             fxmlLoader.<MainWindow>getController().setSiri(siri);  // inject the Siri instance
             stage.show();
         } catch (IOException e) {
